5dd24b5fdd12a5e3794b37a65b71f85e3270050e
Achmad Raffa Adhiyaksa <raffaadhiyaksa2@gmail.com>
2025-11-28 10:29:07 +0700

    Add Dokter structure and list management functions

--- dokter.h
+++ dokter.h
@@ -1,1 +1,35 @@
+#ifndef DOKTER_H
+#define DOKTER_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Pasien;
+
+typedef struct Dokter* adrDokter;
+typedef struct Pasien* adrPasien;
+
+struct Dokter {
+    string idDokter;
+    string nama;
+    string spesialis;
+    adrDokter next;
+    adrPasien firstPasien; 
+};
+
+struct ListDokter {
+    adrDokter first;
+};
+
+void createListDokter(ListDokter &L);
+adrDokter newElmDokter(string id, string nama, string spesialis);
+void insertDokter(ListDokter &L, adrDokter D);
+void deleteDokter(ListDokter &L, string id);
+adrDokter searchDokter(ListDokter L, string id);
+void showSemuaDokter(ListDokter L);
+
+#endif
+
 
