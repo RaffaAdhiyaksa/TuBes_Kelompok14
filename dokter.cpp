0c946869bc94082ea15b770417efc4f6934c30fc
Achmad Raffa Adhiyaksa <raffaadhiyaksa2@gmail.com>
2025-12-05 19:55:21 +0700

    BARU

--- dokter.cpp
+++ dokter.cpp
@@ -1,34 +1,80 @@
 #include "dokter.h"
+#include <iostream>
+
+using namespace std;
 
 void createListDokter(ListDokter &L) {
     L.first = nullptr;
+    L.count = 0;
+    L.namaRumahSakit = "";
+    L.alamat = "";
 }
 
 adrDokter newElmDokter(string id, string nama, string spesialis) {
-    adrDokter P = new Dokter;
-    P->idDokter = id;
-    P->nama = nama;
-    P->spesialis = spesialis;
-    P->next = nullptr;
-    P->firstPasien = nullptr;
-    return P;
+    adrDokter D = new Dokter;
+    D->idDokter = id;
+    D->nama = nama;
+    D->spesialis = spesialis;
+    D->next = nullptr;
+    D->firstPasien = nullptr;
+    return D;
 }
 
-void insertDokter(ListDokter &L, adrDokter P) {
+void tambahDokter(ListDokter &L) {
+    adrDokter newDokter = new Dokter;
+    cout << "Masukkan Nama Dokter: ";
+    cin >> newDokter->nama;
+    cout << "Masukkan ID Dokter: ";
+    cin >> newDokter->idDokter;
+    cout << "Masukkan Spesialis: ";
+    cin >> newDokter->spesialis;
+    
+    newDokter->next = nullptr;
+    newDokter->firstPasien = nullptr;
+    
+    insertDokter(L, newDokter);
+}
+
+void insertDokter(ListDokter &L, adrDokter D) {
     if (L.first == nullptr) {
-        L.first = P;
+        L.first = D;
     } else {
-        adrDokter Q = L.first;
-        while (Q->next != nullptr) {
-            Q = Q->next;
+        adrDokter temp = L.first;
+        while (temp->next != nullptr) {
+            temp = temp->next;
         }
-        Q->next = P;
+        temp->next = D;
+    }
+    L.count++;
+}
+
+adrDokter cariDokter(ListDokter L, string id) {
+    adrDokter P = L.first;
+    while (P != nullptr) {
+        if (P->idDokter == id) {
+            return P;
+        }
+        P = P->next;
+    }
+    return nullptr;
+}
+
+void editDokter(ListDokter L, string id) {
+    adrDokter dokter = cariDokter(L, id);
+    if (dokter) {
+        cout << "Edit Nama Dokter: ";
+        cin >> dokter->nama;
+        cout << "Edit Spesialis: ";
+        cin >> dokter->spesialis;
+        cout << "Dokter berhasil diperbarui!" << endl;
+    } else {
+        cout << "Dokter tidak ditemukan!" << endl;
     }
 }
 
-void deleteDokter(ListDokter &L, string x) {
+void deleteDokter(ListDokter &L, string id) {
     if (L.first == nullptr) {
-        cout << "List kosong" << endl;
+        cout << "Dokter tidak ditemukan!" << endl;
         return;
     }
 
@@ -37,7 +83,7 @@ void deleteDokter(ListDokter &L, string x) {
     bool ketemu = false;
 
     while (P != nullptr && !ketemu) {
-        if (P->idDokter == x) {
+        if (P->idDokter == id) {
             ketemu = true;
         } else {
             prec = P;
@@ -51,17 +97,20 @@ void deleteDokter(ListDokter &L, string x) {
         } else { 
             prec->next = P->next;
         }
+        
         P->next = nullptr;
         delete P;
+        L.count--;
+        cout << "Dokter berhasil dihapus!" << endl;
     } else {
-        cout << "Data dokter tidak ditemukan" << endl;
+        cout << "Dokter tidak ditemukan!" << endl;
     }
 }
 
-adrDokter searchDokter(ListDokter L, string x) {
+adrDokter searchDokter(ListDokter L, string id) {
     adrDokter P = L.first;
     while (P != nullptr) {
-        if (P->idDokter == x) {
+        if (P->idDokter == id) {
             return P;
         }
         P = P->next;
@@ -69,19 +118,54 @@ adrDokter searchDokter(ListDokter L, string x) {
     return nullptr;
 }
 
-void showSemuaDokter(ListDokter L) {
-    if (L.first == nullptr) {
-        cout << "List Dokter kosong" << endl;
-    } else {
-        adrDokter P = L.first;
-        int i = 1;
-        cout << "=== DATA DOKTER ===" << endl;
-        while (P != nullptr) {
-            cout << i << ". " << P->idDokter << " - " << P->nama 
-                 << " (" << P->spesialis << ")" << endl;
-            P = P->next;
-            i++;
+void cariDokterByID(ListDokter L, string id) {
+    adrDokter P = L.first;
+    while (P != nullptr) {
+        if (P->idDokter == id) {
+            cout << "Dokter ditemukan!" << endl;
+            cout << "Nama: " << P->nama << endl;
+            cout << "ID: " << P->idDokter << endl;
+            cout << "Spesialis: " << P->spesialis << endl;
+            return;
         }
-        cout << endl;
+        P = P->next;
     }
+    cout << "Dokter dengan ID " << id << " tidak ditemukan!" << endl;
+}
+
+void cariDokterByNama(ListDokter L, string &nama) {
+    adrDokter P = L.first;
+    bool found = false;
+    while (P != nullptr) {
+        if (P->nama == nama) {
+            cout << "Dokter ditemukan!" << endl;
+            cout << "Nama: " << P->nama << endl;
+            cout << "ID: " << P->idDokter << endl;
+            cout << "Spesialis: " << P->spesialis << endl;
+            found = true;
+        }
+        P = P->next;
+    }
+    if (!found) {
+        cout << "Dokter dengan nama " << nama << " tidak ditemukan!" << endl;
+    }
+}
+
+void showSemuaDokter(ListDokter L) {
+    adrDokter P = L.first;
+    cout << "Daftar Dokter:" << endl;
+    cout << "Nama                ID          Spesialis" << endl;
+    cout << "-----------------------------------------------------" << endl;
+
+    while (P != nullptr) {
+        cout << P->nama;
+        cout << string(20 - P->nama.length(), ' ');
+
+        cout << P->idDokter;
+        cout << string(12 - P->idDokter.length(), ' ');
+
+        cout << P->spesialis << endl;
+        P = P->next;
+    }
+    cout << endl;
 }
